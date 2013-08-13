662aa428df72000d63b6b7477b6686d592e8dbc2
Doi9t <watier.yannick@gmail.com>
2013-08-13 11:55:47 -0400

    Update sortby.py
    Optimisation
    Added:
    Sort by integer (BETA)

--- sortby.py
+++ sortby.py
@@ -3,18 +3,28 @@ import sublime, sublime_plugin, string
 class SbyCommand(sublime_plugin.TextCommand):
 	def run(self, edit, sort = 'length', reversed=False):
 		view = self.view;
-
+		
 		if sort == 'length':
 			for region in view.sel():
 				ligne = view.line(region)  
 				contenue = view.substr(ligne);
-				conteneur = sorted(contenue.split('\n'), key = lambda str: len(str), reverse=reversed);
+				conteneur = sorted(contenue.split('\n'), key=lambda str: len(str), reverse=reversed);
 
-				#ajout du caractere
-				i = 0;
-				for mot in conteneur:
+				for (i, mot) in enumerate(conteneur):
 					conteneur[i] = mot + '\n';
-					i = i + 1;
 
 				chaineFinale = ''.join(map(str, conteneur));
-				view.replace(edit, region, chaineFinale.strip())
+				view.replace(edit, region, chaineFinale.strip());
+
+				
+		if sort == 'integer':
+			for region in view.sel():
+				ligne = view.line(region)  
+				contenue = view.substr(ligne);
+				conteneur = sorted(contenue.split('\n'), key=float ,reverse=reversed);
+				
+				for (i, mot) in enumerate(conteneur):
+					conteneur[i] = mot + '\n';
+
+				chaineFinale = ''.join(map(str, conteneur));
+				view.replace(edit, region, chaineFinale.strip());
