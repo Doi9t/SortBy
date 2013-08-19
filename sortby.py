aa8a8093f362a10470007b0d12174cc307846df0
Yannick Watier <watier.yannick@gmail.com>
2013-08-18 20:01:45 -0400

    - Corrected a bug in sort by number - Added sort by binary,octal,hexadecimal - Minor optimisation of the code

--- sortby.py
+++ sortby.py
@@ -1,37 +1,47 @@
-import sublime, sublime_plugin, string, re
+import sublime, sublime_plugin, string
 
-def isNombre(x):
-	try:
-		float(x)
-		return True;
-	except ValueError:
-		return False;
+bases = {'binary' : 2,'octal' : 8,'decimal' : 10, 'hexadecimal' : 16}
+
+def estBase(x,base):
+    try:
+        int(x, base);
+        return True;
+    except ValueError:
+        return False;
+
+def baseToInteger(x,base):
+	return int(x,base);
 
 class SbyCommand(sublime_plugin.TextCommand):
 	def run(self, edit, sort = 'length', reversed=False):
 		view = self.view;
-		
-		if sort == 'length':
-			for region in view.sel():
-				ligne = view.line(region);
-				listeLignes = view.substr(ligne).splitlines(True);
 
+		for region in view.sel():
+			ligne = view.line(region);
+
+			if sort == 'decimal' or sort == 'octal' or sort == 'hexadecimal' or sort == 'binary': 
+				contenue = view.substr(ligne).splitlines();
+				contenue  = [i for i in contenue if estBase(i, bases[sort])]; 
+				conteneur = sorted(contenue, key=lambda str: baseToInteger(str, bases[sort]), reverse=reversed); 
+
+				if len(conteneur) != 0:
+					conteneur = [str(x) + '\n' for x in conteneur]; #On ajoute \n a chaque element
+					conteneur[-1] = conteneur[-1][:-1]; #On enleve le \n du dernier element
+					chaineFinale = ''.join(map(str, conteneur));
+					view.replace(edit, region, chaineFinale);
+				else:
+					print("SortBy error: No number found !");
+
+
+			if sort == 'length':
+				listeLignes = view.substr(ligne).splitlines(True);
 				#Pour le dernier mot
 				if listeLignes[-1][-1] != '\n':
 					listeLignes[-1] += '\n';
 				
 				conteneur = sorted(listeLignes, key=lambda str: len(str), reverse=reversed);
-				chaineFinale = ''.join(map(str, conteneur));
-				view.replace(edit, region, chaineFinale);
-
-
-		if sort == 'number':
-			for region in view.sel():
-				ligne = view.line(region)  
-				contenue = view.substr(ligne).splitlines();
-				contenue  = [i for i in contenue if isNombre(i)]; #Enleve tout les non nombres
-				conteneur = sorted(contenue, key=float, reverse=reversed); #trie le tableau
-				conteneur = [str(x) + '\n' for x in conteneur]; #On ajoute \n a chaque element
-				conteneur[-1] = conteneur[-1][:-1]; #On enleve le \n du dernier element
-				chaineFinale = ''.join(map(str, conteneur));
-				view.replace(edit, region, chaineFinale);
+				if len(conteneur) != 0:
+					chaineFinale = ''.join(map(str, conteneur));
+					view.replace(edit, region, chaineFinale);
+				else:
+					print("SortBy error: No string found !");
