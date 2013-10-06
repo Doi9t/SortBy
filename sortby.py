cfb247d3f26ae6dda98c855ab579e3ecffc72ff7
Yannick Watier <watier.yannick@gmail.com>
2013-10-06 18:15:32 -0400

    Corrected a bug

--- sortby.py
+++ sortby.py
@@ -9,12 +9,14 @@ def isBase(x,base):
     except ValueError:
         return False;
 
-def baseToInteger(x,base):
+def baseToInteger(x, base):
 	return int(x,base);
 
 def removeNewLine(x):
 	for item in xrange(x.count(u'\n')):
 		x.remove(u'\n');
+	for item in xrange(x.count(u'\r')):
+		x.remove(u'\r');
 
 
 class SrtbyliCommand(sublime_plugin.TextCommand):
@@ -27,33 +29,31 @@ class SrtbyliCommand(sublime_plugin.TextCommand):
 			if sort == 'decimal' or sort == 'octal' or sort == 'hexadecimal' or sort == 'binary': 
 				contenue = view.substr(ligne).splitlines();
 				contenue  = [i for i in contenue if isBase(i, bases[sort])]; 
-				
 				removeNewLine(contenue);
-				
 				conteneur = sorted(contenue, key=lambda str: baseToInteger(str, bases[sort]), reverse=reversed); 
 
 				if len(conteneur) != 0:
 					conteneur = [str(x) + '\n' for x in conteneur];
-					conteneur[-1] = conteneur[-1][:-1]; 
+					conteneur[-1] = conteneur[-1][:-1];
 					chaineFinale = ''.join(map(str, conteneur));
 					view.replace(edit, region, chaineFinale);
+
 				else:
 					print("SortBy error: No number found !");
-
 
 			if sort == 'length' or sort == 'string':
 				listeLignes = view.substr(ligne).splitlines(True);
 
 				if listeLignes[-1][-1] != '\n':
 					listeLignes[-1] += '\n';
-
+					
 				removeNewLine(listeLignes);
-
+				
 				if sort == 'length':
 					conteneur = sorted(listeLignes, key=lambda str: len(str), reverse=reversed);
 				else:
 					conteneur = sorted(listeLignes, key=lambda str: str.lower(), reverse=reversed);
-				
+
 				if len(conteneur) != 0:
 					chaineFinale = ''.join(map(str, conteneur));
 					view.replace(edit, region, chaineFinale);
