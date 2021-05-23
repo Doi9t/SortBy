c3e5c2e20a9f333c5c66264afa8d2cb8e4975a88
doi9t <yannick@watier.ca>
2021-05-22 20:16:38 -0400

    - No longer supporting ST2 with this commit and up - Removed the version check when st2 - Removed the future import - Moved the `writeToView` function into the command class - Formatted the code

--- sortby.py
+++ sortby.py
@@ -12,58 +12,51 @@
 #     See the License for the specific language governing permissions and
 #     limitations under the License.
 
-from __future__ import print_function
-import sublime, sublime_plugin, re
+import re
 from collections import defaultdict
 
-bases = {'binary' : 2,'octal' : 8,'decimal' : 10, 'hexadecimal' : 16}
+import sublime
+import sublime_plugin
 
-#Thanks to Ned Batchelder for this function
-#http://nedbatchelder.com/blog/200712/human_sorting.html
-def sort_naturel(liste):
+bases = {'binary': 2, 'octal': 8, 'decimal': 10, 'hexadecimal': 16}
+
+
+# Thanks to Ned Batchelder for this function
+# http://nedbatchelder.com/blog/200712/human_sorting.html
+def natural_sort(list_to_sort):
     convertion = lambda e: int(e) if e.isdigit() else e.lower()
-    key1 = lambda key: [ convertion(g) for g in re.split('([0-9]+)', key) ]
-    return sorted(liste, key = key1)
+    key1 = lambda key: [convertion(g) for g in re.split('([0-9]+)', key)]
+    return sorted(list_to_sort, key=key1)
+
 
 def putEndLines(arr):
-    if int(sublime.version()) < 3000:
-        arr = [unicode(x.encode('utf-8'), 'utf-8') + '\n' for x in arr]
-    else:
-        arr = [str(x.encode('utf-8'), encoding='utf-8') + '\n' for x in arr]
-
+    arr = [str(x.encode('utf-8'), encoding='utf-8') + '\n' for x in arr]
     arr[-1] = arr[-1][:-1]
     return arr
 
-def writeToView(self, region, conteneur):
-    if len(conteneur) != 0:
-        if self.estSelect:
-            self.view.replace(self.edit, region, ''.join(putEndLines(conteneur)))
-        else:
-            self.view.erase(self.edit, sublime.Region(0, self.view.size()))
-            self.view.insert(self.edit, 0, ''.join(putEndLines(conteneur)))
-    else:
-        print("SortBy error: No string found !")
 
 class SortingObj(object):
     def __init__(self, line, number, base):
         self.line = str(line).strip()
         self.number = number
         self.base = base
+
     def getLine(self):
         return self.line
+
     def getNumber(self):
         if self.number == 0:
             return 0
         else:
             return int(self.number, bases[self.base])
 
-class SrtbyliCommand(sublime_plugin.TextCommand):
 
-    def sortNumbers(self, region, contenue, sort): #Sort numbers with letters
-        conteneur = []
+class SrtbyliCommand(sublime_plugin.TextCommand):
+    def sortNumbers(self, region, lines, sort):  # Sort numbers with letters
+        container = []
         obj = []
 
-        for line in contenue:
+        for line in lines:
             number = ''
             if sort == 'decimal':
                 number = re.findall(r'[+-]?\d+(?:\.\d+)?', line)
@@ -74,68 +67,83 @@ class SrtbyliCommand(sublime_plugin.TextCommand):
             if sort == 'binary':
                 number = re.findall(r'[01]+', line)
 
-            if len(number) > 0: #Ok
+            if len(number) > 0:
                 obj.append(SortingObj(line, number[0], sort))
-            else: #No number found
+            else:  # No number found
                 obj.append(SortingObj(line, 0, sort))
 
         obj.sort(key=lambda x: x.getNumber(), reverse=self.reversed)
 
         for line in obj:
-            conteneur.append(line.getLine())
+            container.append(line.getLine())
 
-        writeToView(self, region, conteneur)
+        self.writeToView(region, container)
 
-    def sortStrings(self, region, contenue, sort): #Sort strings and natural order
+    def sortStrings(self, region, content, sort):  # Sort strings and natural order
+        container = None
+
         if sort == 'length':
             if self.settings.get('length_alphabetically_enabled'):
-                conteneur = defaultdict(list)
+                container = defaultdict(list)
 
-                #Put all values in a defaultdict (Grouped by length)
-                for str in sorted(contenue, key=lambda str: len(str)) :
-                    conteneur[len(str)].append(str)
+                # Put all values in a defaultdict (Grouped by length)
+                for value in sorted(content, key=lambda current_str: len(current_str)):
+                    container[len(value)].append(value)
 
-                #Sort the groups alphabetically 
-                for k, v in conteneur.items():
-                    conteneur[k] = sorted(v, reverse=self.settings.get('length_alphabetically_reversed'))
+                # Sort the groups alphabetically
+                for index, values in container.items():
+                    container[index] = sorted(values, reverse=self.settings.get('length_alphabetically_reversed'))
 
-                #Sort the groups by length
-                conteneur = sorted(conteneur.items(), key=lambda t: t[0], reverse=self.reversed)
+                # Sort the groups by length
+                container = sorted(container.items(), key=lambda t: t[0], reverse=self.reversed)
 
-                conteneurTmp = []
-                for k, v in conteneur:
-                    for str in v:
-                        conteneurTmp.append(str)
-                conteneur = conteneurTmp;
-
+                tempContainer = []
+                for index, values in container:
+                    for value in values:
+                        tempContainer.append(value)
+                container = tempContainer
             else:
-                conteneur = sorted(contenue, key=lambda str: len(str), reverse=self.reversed)
+                container = sorted(content, key=lambda current_str: len(current_str), reverse=self.reversed)
 
         elif sort == 'string':
             if self.settings.get('case_sensitive'):
-                conteneur = sorted(contenue, reverse=self.reversed)
+                container = sorted(content, reverse=self.reversed)
             else:
-                conteneur = sorted(contenue, key=lambda str: str.lower(), reverse=self.reversed)
+                container = sorted(content, key=lambda current_string: current_string.lower(), reverse=self.reversed)
 
         elif sort == 'naturalOrder':
-            conteneur = sort_naturel(contenue)
+            container = natural_sort(content)
 
-        writeToView(self, region, conteneur)
+        if container is not None:
+            self.writeToView(region, container)
 
-    def run(self, edit, sort = 'length', reversed=False):
-        lines = []
+    def writeToView(self, region, container):
+        if len(container) != 0:
+            if self.estSelect:
+                self.view.replace(self.edit, region, ''.join(putEndLines(container)))
+            else:
+                self.view.erase(self.edit, sublime.Region(0, self.view.size()))
+                self.view.insert(self.edit, 0, ''.join(putEndLines(container)))
+        else:
+            print("SortBy error: No string found !")
+
+    def run(self, edit, sort='length', reversed=False):
         self.edit = edit
         view = self.view
         self.reversed = reversed
         SrtbyliCommand.edit = edit
         self.settings = sublime.load_settings("SortBy.sublime-settings")
 
-        if view.sel()[0].empty() and len(view.sel()) == 1: #No selection
+        if view.sel()[0].empty() and len(view.sel()) == 1:  # No selection
             self.estSelect = False
             if sort == 'length' or sort == 'string' or sort == 'naturalOrder':
-                self.sortStrings(view.sel()[-1].end(), [x for x in view.substr(sublime.Region(0, self.view.size())).splitlines() if x != ''], sort)
+                self.sortStrings(view.sel()[-1].end(),
+                                 [x for x in view.substr(sublime.Region(0, self.view.size())).splitlines() if x != ''],
+                                 sort)
             elif sort == 'decimal' or sort == 'octal' or sort == 'hexadecimal' or sort == 'binary':
-                self.sortNumbers(view.sel()[-1].end(), [x for x in view.substr(sublime.Region(0, self.view.size())).splitlines() if x != ''], sort)
+                self.sortNumbers(view.sel()[-1].end(),
+                                 [x for x in view.substr(sublime.Region(0, self.view.size())).splitlines() if x != ''],
+                                 sort)
         else:
             self.estSelect = True
             for region in view.sel():
